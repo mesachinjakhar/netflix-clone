167b913095adb458962afb40151a01ac72ae9587
sachin jakhar <mesachinjakhar@gmail.com>
2024-09-03 01:36:56 +0530

    security issue solved

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
-router.get('/create', async function(req, res, next) {
-  const createdUser = await userModel.create({
-    email: "thebotcmd@gmail.com",
-    password: "Sachin@123"
-  })
-  res.send(createdUser)
-});
-
-router.get('/get', async function(req, res, next) {
-  const getUser = await userModel.find({
-    email: "thebotcmd@gmail.com",
-    password: "Sachin@123"
-  })
-  res.send(getUser)
-});
-
-router.get('/delete', async function(req, res, next) {
-  const deletedUser = await userModel.findOneAndDelete({
-    email: "thebotcmd@gmail.com"
-  })
-  res.send(deletedUser)
-});
-
 router.use((err, req, res, next) => {
   res.send("something gone bad.")
 })
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,1 @@
-var express = require('express');
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://thebotcmd:Sachin123@100xdevs.ms0ex.mongodb.net/codekaro")
-
-const userSchema = mongoose.Schema({
-  email: String,
-  password: String
-})
-
-module.exports = mongoose.model("users", userSchema )
+const express = require("express")
