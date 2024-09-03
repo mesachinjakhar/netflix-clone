57efa1b664e88e6f2de8f5e80b01a3d84944271e
sachin jakhar <mesachinjakhar@gmail.com>
2024-09-04 01:01:20 +0530

    browse page updated.

---
+++ public/javascripts/browseScript.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+      var header = document.getElementsByClassName('header')[0]; 
+
+      if (header) {  
+        if (window.scrollY > 0) {
+          header.style.backgroundColor = '#0c0c0c'; 
+        } else {
+          header.style.backgroundColor = 'transparent';
+        }
+    } else {
+        console.error('Header element with class "header" not found!');
+    }
+    });
+});
